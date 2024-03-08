2be8330da02412ada52807a9077f69f7af3fb882
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2024-03-08 20:38:15 +0530

    initial : created Header

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Routes } from "react-router-dom"
+import Header from "./layouts/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Routes>
+      </Routes>
+    </>
+  )
+}
+
+export default App
