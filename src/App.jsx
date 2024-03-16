cade8e8288c7b5484fb905feb2de24a4d4eb121d
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2024-03-16 20:28:43 +0530

    Day 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes } from "react-router-dom"
 import Header from "./layouts/Header"
 import Home from "./pages/Home"
+import Footer from "./layouts/Footer"
 
 function App() {
 
   return (
     <>
       <Header />
-      <Home/>
+      <Home />
+      <Footer />
     </>
   )
 }
