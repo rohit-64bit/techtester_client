b7290fb83a9016919994b3b18198d5142ce83c66
Rohit Kumar Pandit <77102068+rohit-64bit@users.noreply.github.com>
2024-03-22 19:33:02 +0530

    fix: Added responsiveness to the site

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Logo from '../assets/logo.svg'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 
 const Header = () => {
 
     const location = useLocation()
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const [scrolling, setScrolling] = useState(false)
+    const [scrollPosition, setScrollPosition] = useState(0)
 
     const routes = [
         { name: 'Home', path: '#home' },
@@ -13,19 +18,50 @@ const Header = () => {
         { name: 'Contact Us', path: '#contact' }
     ]
 
+    useEffect(() => {
+
+        let timeout;
+
+        const handleScroll = () => {
+
+            const currentPosition = window.scrollY
+
+            if (currentPosition > scrollPosition || currentPosition < scrollPosition || currentPosition < 1) {
+                setScrolling(true)
+            } else {
+                setScrolling(false)
+            }
+
+            clearTimeout(timeout)
+
+            timeout = setTimeout(() => {
+                setScrolling(false)
+            }, 4000);
+
+        }
+
+        window.addEventListener('scroll', handleScroll)
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+            clearTimeout(timeout)
+        }
+
+    }, [])
+
     return (
         <>
-            <header className='flex justify-between fixed w-full z-10 top-0 items-center px-20 py-5 bg-white'>
+            <header className={scrolling?'flex justify-between fixed w-screen z-10 top-0 items-center px-5 md:px-10 lg:px-20 py-5 bg-white duration-500':'flex justify-between fixed w-screen z-10 top-0 items-center px-5 md:px-10 lg:px-20 py-5 bg-white -translate-y-32 duration-500'}>
 
                 <div className='flex gap-4 items-center'>
-                    <img src={Logo} alt="logo" className='w-16' />
-                    <p className='text-xl font-medium'>
+                    <img src={Logo} alt="logo" className='w-14 md:w-16' />
+                    <p className='text-sm md:text-xl font-medium'>
                         Techtester<br />
                         Infotech
                     </p>
                 </div>
 
-                <menu className='bg-black flex gap-3 rounded-full px-1 py-1'>
+                <menu className='bg-black hidden lg:flex gap-3 rounded-full px-1 py-1'>
 
                     {
                         routes.map((route, index) => {
@@ -37,7 +73,29 @@ const Header = () => {
 
                 </menu>
 
+                <menu onClick={() => {
+                    setIsOpen(!isOpen)
+                }} className='flex flex-col items-center justify-center lg:hidden gap-1 bg-black aspect-square p-4 rounded-full'>
+
+                    <div className={isOpen ? 'border-2 rounded-full border-yellow-400 w-6 translate-y-[4px] rotate-45 duration-500' : 'border-2 rounded-full border-yellow-400 w-6 duration-500'} />
+                    <div className={isOpen ? 'hidden' : 'border-2 rounded-full border-yellow-400 w-6'} />
+                    <div className={isOpen ? '-translate-y-[4px] -rotate-45 border-2 rounded-full border-yellow-400 w-6 duration-500' : 'border-2 rounded-full border-yellow-400 w-6 duration-500'} />
+
+                </menu>
+
             </header>
+
+            <nav className={isOpen ? 'bg-black flex flex-col gap-5 overflow-hidden lg:hidden fixed w-screen z-10 top-24 p-5 duration-500' : 'h-0 flex flex-col overflow-hidden duration-500 p-0'}>
+
+                {
+                    routes.map((route, index) => {
+                        return (
+                            <a onClick={() => setIsOpen(!isOpen)} className={location.hash === route.path ? 'text-lg bg-[#F9C869] text-black px-8 py-1 rounded-lg font-medium transition-all duration-300' : 'text-lg text-[#F9C869] px-8 py-1 font-medium transition-all duration-300'} href={route.path} key={index}>{route.name}</a>
+                        )
+                    })
+                }
+
+            </nav>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import EastIcon from '@mui/icons-material/East';
 import PlaceIcon from '@mui/icons-material/Place';
 import WhatsAppIcon from '@mui/icons-material/WhatsApp';
 import PhoneIcon from '@mui/icons-material/Phone';
+import { Rating } from '@mui/material'
 
 const Home = () => {
 
@@ -69,95 +70,50 @@ const Home = () => {
 
     return (
         <>
+            <main>
 
-            {/* Heroes Section */}
-            <section id='home' className='flex items-center justify-center h-screen px-20'>
+                {/* Heroes Section */}
+                <section id='home' className='flex flex-col md:flex-row items-center justify-center h-screen px-5 md:px-10 lg:px-20 gap-10 md:gap-0 mt-14 md:mt-0'>
 
-                <div className='w-1/2 flex flex-col gap-10'>
-                    <h1 className='text-6xl font-medium'>Techtester Infotech</h1>
-                    <p>
-                        Welcome to <strong>Techtester Infotech</strong>, your IT heroes! We specialize in CCTV maintanece ( IP & normal cameras ),network and router configuration, desktop and laptop repairs, printer servicing, and server maintainance. We are a team of dedicated professionals who are always ready to help you with your IT needs. We are available 24/7, so you can reach out to us anytime. We are here to help you with all your IT needs.
-                    </p>
-                    <a className='bg-black w-max text-[#F9C869] rounded-xl font-medium px-12 py-2' href='#home'>Contact Us</a>
-                </div>
+                    <div className='w-full md:w-1/2 flex flex-col gap-5 md:gap-10'>
+                        <h1 className='text-3xl lg:text-6xl font-medium'>Techtester Infotech</h1>
+                        <p>
+                            Welcome to <strong>Techtester Infotech</strong>, your IT heroes! We specialize in CCTV maintanece ( IP & normal cameras ), network and router configuration, desktop and laptop repairs, printer servicing, and server maintainance. We are a team of dedicated professionals who are always ready to help you with your IT needs. We are available 24/7, so you can reach out to us anytime. We are here to help you with all your IT needs.
+                        </p>
+                        <a className='bg-black w-max text-[#F9C869] rounded-xl font-medium px-12 py-2' href='#contact'>Contact Us</a>
+                    </div>
 
-                <div className='w-1/2 flex items-baseline justify-end'>
+                    <div className='w-full md:w-1/2 flex justify-center items-center md:items-baseline md:justify-end'>
 
-                    <img src={HeroImg} className='w-[80%] mt-32' alt="" />
+                        <img src={HeroImg} className='w-[80%] md:mt-32' alt="" />
 
-                </div>
+                    </div>
 
-            </section>
+                </section>
 
-            {/* Sales Section */}
-            <section id='sales' className='h-screen bg-black px-20 py-10 flex flex-col gap-10'>
+                {/* Sales Section */}
+                <section id='sales' className='max-h-max min-h-screen bg-black px-5 md:px-10 lg:px-20 py-10 flex flex-col gap-10'>
 
-                <h2 className='text-2xl text-[#F9C869] font-medium'>What we sell ?</h2>
+                    <h2 className='text-xl md:text-2xl text-[#F9C869] font-medium'>What we sell ?</h2>
 
-                <div className='grid grid-cols-3 h-full w-full gap-10'>
-
-                    {
-                        subHeroData.map((data, index) => {
-                            return (
-                                <div key={index} className='w-full h-full flex flex-col justify-around p-5 bg-black custom-shadow rounded-xl'>
-                                    <img src={data.img} className='w-full object-contain' />
-                                    <h2 className='text-2xl text-[#F9C869] font-medium'>{data.title}</h2>
-                                    <p className='text-white'>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptates facilis alias adipisci temporibus laboriosam sed excepturi sunt explicabo animi repudiandae mollitia officiis, facere sit sapiente? Odit, ipsa fuga. Necessitatibus!
-                                    </p>
-                                    <a href="#contact" className='w-max flex items-center justify-around gap-2 px-10 py-2 rounded-lg outline outline-2 outline-[#F9C869] text-[#F9C869] hover:text-black hover:outline-black hover:bg-[#F9C869] transition-all ease-in-out duration-300'>
-
-                                        <div className='text-xl font-medium w-max'>Contact Us</div>
-
-                                        <EastIcon />
-
-                                    </a>
-                                </div>
-                            )
-                        })
-                    }
-
-                </div>
-
-            </section>
-
-            {/* Services Section */}
-            <section id='services' className='flex flex-col gap-10 items-center justify-center h-screen px-20 min-h-screen max-h-max'>
-
-                <h2 className='text-2xl font-medium text-center'>Our Services</h2>
-
-                <div className='grid grid-cols-3 gap-10 w-full'>
-
-                    {
-                        serviceData?.map((data, index) => {
-                            return (
-                                <div key={index} className='card-shadow rounded-xl w-full p-10'>
-                                    <h4 className='font-medium text-xl mb-5'>{data.title}</h4>
-                                    <p>{data.description}</p>
-                                </div>
-                            )
-                        })
-                    }
-
-                </div>
-
-            </section>
-
-            {/* Contact Section */}
-            <section id='contact' className='bg-black px-20 py-10 flex justify-between'>
-
-                <div className='flex flex-col gap-10 w-[20%]'>
-
-                    <h2 className='text-2xl font-medium text-white'>Contact Us</h2>
-
-                    <div className='flex flex-col gap-5'>
+                    <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 h-full w-full gap-5 md:gap-10'>
 
                         {
-                            contactDetails?.map((data, index) => {
+                            subHeroData.map((data, index) => {
                                 return (
-                                    <div key={index} className='flex items-center gap-5'>
-                                        <data.icon fontSize='large' className='text-white' />
-                                        <p className='text-white text-lg'>{data.description}</p>
+                                    <div key={index} className='w-full h-full flex flex-col justify-around p-5 bg-black custom-shadow rounded-xl gap-2'>
+                                        <img src={data.img} className='w-full object-contain' />
+                                        <h2 className='text-xl md:text-2xl text-[#F9C869] font-medium'>{data.title}</h2>
+                                        <p className='text-white text-sm'>
+                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptates facilis alias adipisci temporibus laboriosam sed excepturi sunt explicabo animi repudiandae mollitia officiis, facere sit sapiente? Odit, ipsa fuga. Necessitatibus!
+                                        </p>
+                                        <a href="#contact" className='w-max flex items-center justify-around gap-2 px-10 py-2 rounded-lg outline outline-2 outline-[#F9C869] text-[#F9C869] hover:text-black hover:outline-black hover:bg-[#F9C869] transition-all ease-in-out duration-300'>
+
+                                            <div className='text-xl font-medium w-max'>Contact Us</div>
+
+                                            <EastIcon />
+
+                                        </a>
                                     </div>
                                 )
                             })
@@ -165,26 +121,75 @@ const Home = () => {
 
                     </div>
 
-                </div>
+                </section>
 
-                {/* Contact Form */}
-                <form className='flex flex-col gap-2 w-[30%]'>
+                {/* Services Section */}
+                <section id='services' className='flex flex-col gap-10 items-center justify-center px-5 md:px-10 lg:px-20 py-10 min-h-screen max-h-max'>
 
-                    <p className='text-white text-lg mb-5'>Submit your contact details our team will contact you soon.</p>
+                    <h2 className='text-xl md:text-2xl font-medium text-center'>Our Services</h2>
 
-                    <input className='outline-none border-none px-4 py-3 bg-[#D9D9D9] font-medium rounded-xl text-lg' type="text" placeholder='Name' />
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full'>
 
-                    <input className='outline-none border-none px-4 py-3 bg-[#D9D9D9] font-medium rounded-xl text-lg' type="email" placeholder='Email' />
+                        {
+                            serviceData?.map((data, index) => {
+                                return (
+                                    <div key={index} className='card-shadow rounded-xl w-full p-10'>
+                                        <h4 className='font-medium text-xl mb-2 md:mb-5'>{data.title}</h4>
+                                        <p className='text-sm md:text-base'>{data.description}</p>
+                                    </div>
+                                )
+                            })
+                        }
 
-                    <input className='outline-none border-none px-4 py-3 bg-[#D9D9D9] font-medium rounded-xl text-lg' type="text" pattern="^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$" placeholder="Contact No." />
+                    </div>
 
-                    <button className='bg-[#F9C869] text-black text-lg rounded-xl font-medium w-[40%] py-2'>
-                        Submit
-                    </button>
+                </section>
 
-                </form>
-            </section>
+                <Rating name="read-only" value={3} readOnly />
 
+                {/* Contact Section */}
+                <section id='contact' className='bg-black px-8 md:px-10 lg:px-20 py-10 flex flex-col md:flex-row gap-10 md:gap-0 justify-center items-center md:justify-between h-screen'>
+
+                    <div className='flex flex-col gap-10 w-full lg:w-[20%]'>
+
+                        <h2 className='text-lg md:text-2xl font-medium text-white'>Contact Us</h2>
+
+                        <div className='flex flex-col gap-2 md:gap-5'>
+
+                            {
+                                contactDetails?.map((data, index) => {
+                                    return (
+                                        <div key={index} className='flex items-center gap-2 md:gap-5'>
+                                            <data.icon fontSize='large' className='text-white' />
+                                            <p className='text-white text-lg'>{data.description}</p>
+                                        </div>
+                                    )
+                                })
+                            }
+
+                        </div>
+
+                    </div>
+
+                    {/* Contact Form */}
+                    <form className='flex flex-col gap-2 w-full lg:w-[30%]'>
+
+                        <p className='text-white text-lg mb-5'>Submit your contact details our team will contact you soon.</p>
+
+                        <input className='outline-none border-none px-4 py-3 bg-[#D9D9D9] font-medium rounded-xl text-lg' type="text" placeholder='Name' />
+
+                        <input className='outline-none border-none px-4 py-3 bg-[#D9D9D9] font-medium rounded-xl text-lg' type="email" placeholder='Email' />
+
+                        <input className='outline-none border-none px-4 py-3 bg-[#D9D9D9] font-medium rounded-xl text-lg' type="text" pattern="^(?:(?:\+|0{0,2})91(\s*|[\-])?|[0]?)?([6789]\d{2}([ -]?)\d{3}([ -]?)\d{4})$" placeholder="Contact No." />
+
+                        <button className='bg-[#F9C869] text-black text-lg rounded-xl font-medium w-[40%] py-2'>
+                            Submit
+                        </button>
+
+                    </form>
+                </section>
+
+            </main>
         </>
     )
 }
